2b73f4e6dd5d6d879c5b4dc9723917a3c6d641d1
Zhan Gao <zhangao@usc.edu>
2024-05-27 16:37:47 +0800

    vectorize date_stamp.R

--- date_stamp.R
+++ date_stamp.R
@@ -1,26 +1,19 @@
 date.stamp <- function(stat, cv, delta = 0.8){
   
-  # a "stupid" function to conduct the date-stamp strategy
-  dif <- stat - cv
-  dif.temp <- cumsum(dif > 0)
-  bb.ind <- c(0, dif.temp[-1] - dif.temp[-length(dif.temp)])
-  period.min <- floor(delta * log(n))
-  
-  i <- 1
-  stamp = NULL
-  while(i < length(bb.ind)){
-    if(bb.ind[i] == 1){
-      j <- i
-      while(j < length(bb.ind)){
-        if(bb.ind[j+1] == 1) j <- j + 1
-        else break
-      }
-      if(j - i + 1 >= period.min) stamp <- rbind(stamp, c(i, j))
-      i <- j + 1
-    }
-    else{
-      i <- i +1
-    }
-  }
-  return(stamp)
+    # Calculate the difference and identify periods where stat is greater than cv
+    dif <- stat > cv
+    
+    # Identify changes in the status of dif to find beginnings and ends of periods
+    changes <- which(diff(c(FALSE, dif, FALSE)) != 0)
+    starts <- changes[seq(1, length(changes), by = 2)]
+    ends <- changes[seq(2, length(changes), by = 2)] - 1
+    
+    # Calculate the minimum period length based on the total number of observations
+    period.min <- floor(delta * log(length(stat)))
+    
+    # Filter periods by minimum length
+    valid <- (ends - starts + 1) >= period.min
+    stamp <- cbind(starts[valid], ends[valid])
+    
+    return(stamp)
 }
